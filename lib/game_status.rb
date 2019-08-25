1b29861ffed886dba3cea82fe9e1b499f2955c01
Andrew Williams <1williamsaj@gmail.com>
2019-08-25 01:26:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,7 +30,7 @@ def won?(board)
     win_index_2 = win_combination[1] #Next, it does the same for the value in that array at [1]
     win_index_3 = win_combination[2] #Finally, the value at [2] is assigned, allowing our code below to analyze the contents of each sub-array.
     # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+    # The code below takes our game board array and checks to see if any combination exists that is a winning match.
     board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] && board[win_index_1] != " "
-  end
+  end #
 end
