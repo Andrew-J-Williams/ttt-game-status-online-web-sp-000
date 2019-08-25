699fb830cf8cceacdfce792542c287457d141792
Andrew Williams <1williamsaj@gmail.com>
2019-08-25 02:31:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -53,8 +53,8 @@ end
 
 def over?(board)
 
-  if won?(board) || full?(board) #returns true if the board has a winning move or the board is full with no winning moves. 
-    return true
+  if won?(board) || full?(board) #returns true if the board has a winning move or the board is full with no winning moves.
+    return true #Will return true if the game is over.
   end
 
 end
